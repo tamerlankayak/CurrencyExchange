04eb108d26e1c941e7706ca5d5dfbd11a551f8f0
Tamerlan <tamerlan_94@mail.ru>
2021-10-07 00:49:15 +0400

    first commit

---
+++ app/src/main/java/com/example/currencyexchange/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.currencyexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//api key 98108816f30be67d1a9134ef6c949507
