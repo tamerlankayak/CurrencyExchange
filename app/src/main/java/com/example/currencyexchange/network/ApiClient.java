d4919e679e05fbaa6b9b926821ba62721365d467
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-02-26 23:44:04 +0400

    Update ApiClient.java
    Some comments

--- app/src/main/java/com/example/currencyexchange/network/ApiClient.java
+++ app/src/main/java/com/example/currencyexchange/network/ApiClient.java
@@ -10,6 +10,7 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+//For http requests using Retrofit library
 public class ApiClient {
 
     public static Retrofit retrofit = null;
